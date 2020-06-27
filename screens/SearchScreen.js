558570722af2ccbd4f13ac97b4580e5f229c180d
Abhinav Sriram <sriramabhinav2112@gmail.com>
2020-06-28 00:16:08 +0530

    Add files via upload

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,11 +1,144 @@
+/*–––––––––––––––––––––––––REACT IMPORTS–––––––––––––––––––––––––*/
 import React, { Component } from "react";
 import { StyleSheet, Text, View, ScrollView } from "react-native";
-
-import Icon from "react-native-vector-icons/Ionicons";
 import { TextInput } from "react-native-gesture-handler";
 import { Header } from "react-native-elements";
 
+/*–––––––––––––––––––––––––FIREBASE IMPORT–––––––––––––––––––––––––*/
+import * as firebase from "firebase";
+import "firebase/firestore";
+
+/*–––––––––––––––––––––––––ICONS IMPORT–––––––––––––––––––––––––*/
+import Icon from "react-native-vector-icons/Ionicons";
+
+/*–––––––––––––––––––––––––CUSTOM IMPORTS–––––––––––––––––––––––––*/
+import courseData from "./../data/spring2020CourseInformation.json";
+
+/*–––––––––––––––––––––––––SEARCH SCREEN COMPONENT–––––––––––––––––––––––––*/
 class SearchScreen extends Component {
+  state = {
+    searchValue: "",
+  };
+
+  // 1614
+
+  getPropertyByIndex = (obj, index) => {
+    return obj[Object.keys(obj)[index]];
+  };
+
+  createKeywordArrayMaster = () => {
+    let courseCodeList = [];
+    let courseNameList = [];
+    let courseInstrList = [];
+    for (let i = 0; i < Object.keys(courseData).length; i++) {
+      const courseCode = this.getPropertyByIndex(courseData, i)["Course Code"];
+      const courseName = this.getPropertyByIndex(courseData, i)["Course Name"];
+      const courseInstr = this.getPropertyByIndex(courseData, i)[
+        "Course Instr"
+      ];
+      courseCodeList.push(courseCode);
+      courseNameList.push(courseName);
+      courseInstrList.push(courseInstr);
+    }
+    // loop
+    // course.keywords = createKeywordArray([courseCodeList[i], courseNameList[i], courseInstrList[i]])
+
+    // for (let j = 100; j < 500; j++) {
+    //   this.writeToDatabase(courseCodeList[j]);
+    // }
+  };
+
+  createKeywordArrayHelper = (toSplit) => {
+    const keywordArray = [];
+    let currKeyword = "";
+    toSplit.split("").forEach((toSplitChar) => {
+      currKeyword += toSplitChar;
+      keywordArray.push(currKeyword);
+    });
+    return keywordArray;
+  };
+
+  createKeywordArray = (toSplit) => {
+    const [courseCode, courseName, courseInstr] = toSplit;
+    const keywordArrayCourseCodeWithSpace = this.createKeywordArrayHelper(
+      courseCode.toLowerCase()
+    );
+    const keywordArrayCourseCodeWithoutSpace = this.createKeywordArrayHelper(
+      courseCode.split(" ").join("").toLowerCase()
+    );
+    const keywordArrayCourseName = this.createKeywordArrayHelper(
+      courseName.toLowerCase()
+    );
+    const keywordArrayCourseInstrFullName = this.createKeywordArrayHelper(
+      courseInstr.toLowerCase()
+    );
+    const keywordArrayCourseInstrLastName = this.createKeywordArrayHelper(
+      courseInstr.split(" ").pop().toLowerCase()
+    );
+    return [
+      ...new Set([
+        "",
+        ...keywordArrayCourseCodeWithSpace,
+        ...keywordArrayCourseCodeWithoutSpace,
+        ...keywordArrayCourseName,
+        ...keywordArrayCourseInstrFullName,
+        ...keywordArrayCourseInstrLastName,
+      ]),
+    ];
+  };
+
+  readFromDatabase = (courseCode) => {
+    const keywordArray = [];
+    keywordArray.push(courseCode);
+    var courseName = "";
+    var courseInstr = "";
+    firebase
+      .firestore()
+      .collection("spring-2020")
+      .doc(courseCode)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          courseName = doc.data()["Course Name"];
+          courseInstr = doc.data()["Course Instructor"];
+          keywordArray.push(courseName);
+          keywordArray.push(courseInstr);
+        } else {
+          console.log("Error");
+        }
+      });
+    return keywordArray;
+  };
+
+  writeToDatabase = (courseCode) => {
+    const keywordArray = this.readFromDatabase(courseCode);
+    setTimeout(() => {
+      firebase
+        .firestore()
+        .collection("spring-2020")
+        .doc(courseCode)
+        .set(
+          {
+            Keywords: this.createKeywordArray(keywordArray),
+          },
+          { merge: true }
+        );
+    }, 5000);
+  };
+
+  searchEngine = async (searchInput) => {
+    const querySnapshot = await firebase
+      .firestore()
+      .collection("spring-2020")
+      .where("Keywords", "array-contains", searchInput.toLowerCase())
+      .orderBy("Course_Code")
+      .get();
+    const result = querySnapshot.docs;
+    result.forEach((course) => {
+      console.log(course.get("Course_Code"));
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -17,9 +150,15 @@ class SearchScreen extends Component {
         <View style={styles.searchBox}>
           <Icon name="ios-search" size={20} />
           <TextInput
-            placeholder="Course Code, Name, CRN, Field, Instructor "
+            placeholder="Course Code, Name, Field, Instructor "
             placeholderTextColor="dimgrey"
             style={styles.textInput}
+            onChangeText={(text) => this.setState({ searchValue: text })}
+            value={this.state.searchValue}
+            onKeyPress={async () => {
+              await this.searchEngine(this.state.searchValue);
+            }}
+            // onKeyPress={() => this.createKeywordArrayMaster()}
           />
         </View>
         <ScrollView contentContainerStyle={styles.text}></ScrollView>
@@ -28,6 +167,7 @@ class SearchScreen extends Component {
   }
 }
 
+/*–––––––––––––––––––––––––SEARCH SCREEN STYLING–––––––––––––––––––––––––*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
