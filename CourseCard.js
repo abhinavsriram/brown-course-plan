8cbaf6fd0b44b80f3c30096af786364e71e4b478
Abhinav Sriram <sriramabhinav2112@gmail.com>
2020-06-28 23:05:28 +0530

    Add files via upload

---
+++ CourseCard.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+/*–––––––––––––––––––––––––COURSE CARD COMPONENT–––––––––––––––––––––––––*/
+const CourseCard = ({
+  onPress,
+  courseCode,
+  courseName,
+  grading,
+  credit,
+  concentrationRequirement,
+  writRequirement,
+}) => (
+  <View style={styles.mainContainer}>
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={0.4}
+      style={[
+        styles.courseCard,
+        {
+          borderColor:
+            "rgb(" +
+            Math.floor(Math.random() * 256) +
+            "," +
+            Math.floor(Math.random() * 256) +
+            "," +
+            Math.floor(Math.random() * 256) +
+            ")",
+        },
+      ]}
+    >
+      <View style={styles.container}>
+        <Text style={styles.line1}>{courseCode}</Text>
+        <Text numberOfLines={1} style={styles.line2}>
+          {courseName}
+        </Text>
+        <View style={styles.line3Container}>
+          <Text style={styles.line3}>{grading} | </Text>
+          <Text style={styles.line3}>{credit}</Text>
+        </View>
+        <View style={styles.line4Container}>
+          <Text style={styles.line4}>{concentrationRequirement} | </Text>
+          <Text style={styles.line4}>{writRequirement}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  </View>
+);
+
+/*–––––––––––––––––––––––––STYLING FOR COURSE CARD COMPONENT–––––––––––––––––––––––––*/
+const styles = StyleSheet.create({
+  mainContainer: {
+    marginBottom: 7,
+  },
+  courseCard: {
+    height: 100,
+    backgroundColor: "#fafafa",
+    borderWidth: 3,
+    borderRadius: 10,
+    width: 0.85 * Dimensions.get("window").width,
+  },
+  container: {
+    flex: 1,
+    padding: 5,
+  },
+  line1: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#484747",
+    padding: 1,
+  },
+  line2: {
+    fontSize: 15,
+    color: "#484747",
+    padding: 1,
+  },
+  line3Container: {
+    flex: 1,
+    flexDirection: "row",
+    paddingTop: 4,
+  },
+  line3: {
+    fontSize: 13,
+    color: "#757575",
+    padding: 1,
+    fontStyle: "italic",
+  },
+  line4Container: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  line4: {
+    fontSize: 13,
+    color: "#757575",
+    padding: 1,
+  },
+});
+
+export default CourseCard;
---
+++ SemesterCard.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button, Dimensions } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+import CourseCard from "./../components/CourseCard";
+import AddCourseCard from "./../components/AddCourseCard";
+
+const SemesterCard = ({ title }) => (
+  <View style={styles.mainContainer}>
+    <Text style={styles.title}>{title}</Text>
+    <Text></Text>
+    <CourseCard
+      courseCode="CSCI 0160"
+      courseName="Introduction to Algorithms and Data Structures"
+      grading="Graded A/B/C/NC"
+      credit="1 Credit"
+      concentrationRequirement="Concentration Requirement"
+      writRequirement="WRIT Requirement"
+    ></CourseCard>
+    <CourseCard
+      courseCode="ECON 1130"
+      courseName="Intermediate Microeconomics (Mathematical) This name is long"
+      grading="Graded A/B/C/NC"
+      credit="1 Credit"
+      writRequirement="WRIT Requirement"
+    ></CourseCard>
+    <AddCourseCard></AddCourseCard>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    borderColor: "#E3E3E3",
+    borderWidth: 3,
+    borderRadius: 15,
+    width: 0.9 * Dimensions.get("window").width,
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  title: {
+    marginLeft: 10,
+    marginTop: 12,
+    color: "#757575",
+    fontSize: 24,
+    fontWeight: "800",
+    alignSelf: "stretch",
+    justifyContent: "space-around",
+  },
+  addCourseCard: {
+    height: 100,
+    backgroundColor: "#fafafa",
+    borderColor: "#E3E3E3",
+    borderWidth: 3,
+    borderRadius: 10,
+    borderStyle: "dotted",
+    width: 0.85 * Dimensions.get("window").width,
+    marginBottom: 7,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addCourse: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#ABABAB",
+  },
+});
+
+export default SemesterCard;
