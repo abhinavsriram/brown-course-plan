d6b93374bc09d61d6e3bb6d295bd52556d2e466d
Abhinav Sriram <sriramabhinav2112@gmail.com>
2020-08-07 13:12:43 +0530

    remove api key

--- App.js
+++ App.js
@@ -28,17 +28,6 @@ if (!global.atob) {
 // confidential information - apiKey and appId
 import * as firebase from "firebase";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDIS2EvIGVPsNV3f5b61pBrL6Vce7-In68",
-  authDomain: "brown-cp.firebaseapp.com",
-  databaseURL: "https://brown-cp.firebaseio.com",
-  projectId: "brown-cp",
-  storageBucket: "brown-cp.appspot.com",
-  messagingSenderId: "522825335797",
-  appId: "1:522825335797:web:52d33920d5e2efa15d16bc",
-  measurementId: "G-R9CV6FQFHX",
-};
-
 firebase.initializeApp(firebaseConfig);
 
 const AppSwitchNavigator = createSwitchNavigator(
